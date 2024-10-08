0437e75100ba1d8fa2971d5b91c0fd2b1951cf56
AbnerWender <abnderwender@gmail.com>
2024-10-08 12:07:15 -0300

    conexao completa com o banco de dados

--- index.php
+++ index.php
@@ -3,14 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="style.css">
     <title>Funcionario</title>
 </head>
 <body>
-    <form action="" id="form-cadastro" >
+    
+    <form action="" id="form-cadastro" method="post">
         <div class="row">
             <label for="nome">Nome: </label>
-            <input type="text" id="nome">
+            <input type="text" id="nome" name='nome'>
         </div>
 
         <div class="row">
@@ -38,39 +39,94 @@
         </div>
     </form>
 
+    <section id="secao-select">
+        <form action="" id='select-todos' method="get">
+            <label for="mostre-todos">Filtrar</label>
+            <select name="cargo" id="select-cargo">
+                <option value="treinee">Treinee</option>
+                <option value="devJunior">Dev Junior</option>
+                <option value="devSenior">Dev Senior</option>
+            </select>
+            <input type="submit" name="btn-mostrar" value="Mostrar" id="mostrar">
+        </form>
+    </section>
     
     <?php
-        //entender qual o erro desta parte 
-        $nome = $_GET['nome'];
-        $cargo = $_GET['cargo'];
-        $salario = $_GET['salario'];
-        $idade = $_GET['idade'];
-        $telefone = $_GET['telefone'];
-        
-        function renderTeamplate($funcionario){
-            include 'teamplate.php';
-        }
-        
         $servidor = 'localhost';
         $usuario = 'root';
         $senha = '';
         $banco_de_dados = 'empresa_abnerdb';
+
+        $conexao = mysqli_connect($servidor, $usuario, $senha, $banco_de_dados);
+
+        if(isset($_POST['nome']) && isset($_POST['cargo']) && isset($_POST['salario']) && isset($_POST['idade']) && isset($_POST['telefone'])){
+            $nome = $_POST['nome'];
+            $cargo = $_POST['cargo'];
+            $salario = $_POST['salario'];
+            $idade = $_POST['idade'];
+            $telefone = $_POST['telefone'];
+            
+            $insert = $conexao->query("insert into funcionario(nome, cargo, idade, salario, telefone) values('$nome', '$cargo', '$idade', '$salario', '$telefone')");
+            
+            $conexao->close();
+        }
+
+        $conexao = mysqli_connect($servidor, $usuario, $senha, $banco_de_dados);
+
+        // if(isset($_GET['select-cargo'])){
+        // function renderTeamplate($funcionario){
+        //     include 'teamplate.php';
+        // };
+
+        // $cargo = $_GET['select-cargo'];
+        // $selectFiltro = $conexao->query('select * from funcionario where '$cargo' =   )');
+        
+        // $rowsFuncionario = $selectTodos->fetch_all(MYSQLI_ASSOC);
+        
+        // $conexao->close();
+        
+        // foreach($rowsFuncionario as $funcionario){
+        //     renderTeamplate($funcionario);
+        // };}
         
         $conexao = mysqli_connect($servidor, $usuario, $senha, $banco_de_dados);
+
+        function renderTeamplate($funcionario){
+            include 'teamplate.php';
+        }
+
+        $selectTodos = $conexao->query('select * from funcionario');
         
-        // $consulta = $conexao->query('insert into funcionario(id, nome, cargo, idade, salario, telefone) values(3,"Everton Silva","Analista", 18, 10000, "67988888888")');
-        
-        $selectFuncionario = $conexao->query('select * from funcionario');
-        
-        $rowsFuncionario = $selectFuncionario->fetch_all(MYSQLI_ASSOC);
+        $rowsFuncionario = $selectTodos->fetch_all(MYSQLI_ASSOC);
         
         $conexao->close();
         
         foreach($rowsFuncionario as $funcionario){
             renderTeamplate($funcionario);
         };
+
+        ?>
+
+        ?>
+
+
+
+        $conexao = mysqli_connect($servidor, $usuario, $senha, $banco_de_dados);
+
+        function renderTeamplate($funcionario){
+            include 'teamplate.php';
+        }
+
+        $selectTodos = $conexao->query('select * from funcionario');
         
+        $rowsFuncionario = $selectTodos->fetch_all(MYSQLI_ASSOC);
+        
+        $conexao->close();
+        
+        foreach($rowsFuncionario as $funcionario){
+            renderTeamplate($funcionario);
+        };
+
         ?>
 </body>
 </html>
-
--- teamplate.php
+++ teamplate.php
@@ -1,12 +1,15 @@
-<div class="card-funcionario">
-    <header>
-        <h2>Nome: <?= $funcionario['nome']; ?> </h2>
-        <p>Cargo: <?= $funcionario['cargo']; ?> </p>
-        <p>Idade: <?= $funcionario['idade']; ?> </p>
-    </header>
-
-    <footer>
-        <a href="tel:">Telefone: <?= $funcionario['telefone']; ?></a>
-        <span>ID: <?= $funcionario['id']; ?></span>
-    </footer>
-</div>
+<section id="secao-funcionario">
+    <div class="card-funcionario">
+        <header>
+            <h2>Nome: <?= $funcionario['nome']; ?> </h2>
+            <p>Cargo: <?= $funcionario['cargo']; ?> </p>
+            <p>Idade: <?= $funcionario['idade']; ?> </p>
+        </header>
+        
+        <footer>
+            <span>ID: <?= $funcionario['id']; ?></span>
+            <br>
+            <a href="tel:">Telefone: <?= $funcionario['telefone']; ?></a>
+        </footer>
+    </div>
+</section>
